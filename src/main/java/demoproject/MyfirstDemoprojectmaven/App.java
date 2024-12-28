08686188675ffc44fd328c4f1bec4c6d592a9510
jittu123ku <jittu.90099@gmail.com>
2024-12-29 00:36:30 +0530

    added new version of code

--- src/main/java/demoproject/MyfirstDemoprojectmaven/App.java
+++ src/main/java/demoproject/MyfirstDemoprojectmaven/App.java
@@ -9,6 +9,7 @@ public class App
 	public void addData(String data)
 	{
 		System.out.println("Data = "+data);
+		System.out.println("This method is for test purpose");
 	}
     public static void main( String[] args )
     {
